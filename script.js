81dea063b5ee037194bd4cd62ddee8a1d66153bf
Anjali Rajeev <anjalirajeev.design@gmail.com>
2025-05-04 12:24:42 +0200

    Replace random image with uploaded image and generate palette

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 const imageUploader = document.getElementById("imageUploader");
-const uploadedImage = document.getElementById("uploadedImage");
 const paletteDiv = document.getElementById("palette");
 const exportBtn = document.getElementById("exportBtn");
 
@@ -34,11 +33,14 @@ imageUploader.addEventListener("change", (e) => {
   const file = e.target.files[0];
   if (file) {
     const reader = new FileReader();
+
     reader.onload = function(event) {
-      uploadedImage.src = event.target.result;
-      uploadedImage.style.display = "block";
-      uploadedImage.onload = function() {
-        generatePalette(uploadedImage);
+      const placeholderImage = document.getElementById("placeholderImage");
+      placeholderImage.src = event.target.result;
+      placeholderImage.style.display = "block";
+
+      placeholderImage.onload = function() {
+        generatePalette(placeholderImage);
       };
     };
     reader.readAsDataURL(file);
@@ -46,22 +48,12 @@ imageUploader.addEventListener("change", (e) => {
 });
 
 const customImages = [
-  "https://github.com/a-rajeev/palette-generator/blob/update_style/assets/images/Img%20(1).jpg?raw=true"
-  "https://github.com/a-rajeev/palette-generator/blob/update_style/assets/images/Img%20(10).jpeg?raw=true"
-  "https://github.com/a-rajeev/palette-generator/blob/update_style/assets/images/Img%20(11).jpeg?raw=true"
-  "https://github.com/a-rajeev/palette-generator/blob/update_style/assets/images/Img%20(12).jpeg?raw=true"    
-  "https://github.com/a-rajeev/palette-generator/blob/update_style/assets/images/Img%20(13).jpeg?raw=true"
-  "https://github.com/a-rajeev/palette-generator/blob/update_style/assets/images/Img%20(14).jpeg?raw=true"
-  "https://github.com/a-rajeev/palette-generator/blob/update_style/assets/images/Img%20(2).jpeg?raw=true"
-  "https://github.com/a-rajeev/palette-generator/blob/update_style/assets/images/Img%20(2).jpg?raw=true"
-  "https://github.com/a-rajeev/palette-generator/blob/update_style/assets/images/Img%20(3).jpeg?raw=true"
-  "https://github.com/a-rajeev/palette-generator/blob/update_style/assets/images/Img%20(3).jpg?raw=true"
-  "https://github.com/a-rajeev/palette-generator/blob/update_style/assets/images/Img%20(4).jpeg?raw=true"
-  "https://github.com/a-rajeev/palette-generator/blob/update_style/assets/images/Img%20(5).jpeg?raw=true"
-  "https://github.com/a-rajeev/palette-generator/blob/update_style/assets/images/Img%20(6).jpeg?raw=true"
-  "https://github.com/a-rajeev/palette-generator/blob/update_style/assets/images/Img%20(7).jpeg?raw=true"
+  "https://github.com/a-rajeev/palette-generator/blob/update_style/assets/images/Img%20(1).jpg?raw=true",
+  "https://github.com/a-rajeev/palette-generator/blob/update_style/assets/images/Img%20(11).jpeg?raw=true",
+  "https://github.com/a-rajeev/palette-generator/blob/update_style/assets/images/Img%20(14).jpeg?raw=true",
+  "https://github.com/a-rajeev/palette-generator/blob/update_style/assets/images/Img%20(2).jpg?raw=true",
+  "https://github.com/a-rajeev/palette-generator/blob/update_style/assets/images/Img%20(3).jpg?raw=true",
   "https://github.com/a-rajeev/palette-generator/blob/update_style/assets/images/Img%20(8).jpeg?raw=true"
-  "https://github.com/a-rajeev/palette-generator/blob/update_style/assets/images/Img%20(9).jpeg?raw=true"
 ];
 
 const randomImage = customImages[Math.floor(Math.random() * customImages.length)];
